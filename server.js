92e32b57cf8f49c52350d91ca054d489692d0778
Bojurie Rogers-Wright <40696649+Bojurie@users.noreply.github.com>
2020-09-07 08:16:47 -0700

    update

--- server.js
+++ server.js
@@ -82,15 +82,6 @@ app.post('/contact', (req, res) => {
   })
 })
 
-
-
-// const normalizePort = port => parseInt(port, 10);
-// const PORT = normalizePort(process.env.PORT || 5000);
-// server.listen(PORT, err => {
-//   if(err) throw err;
-//   console.log('Server Started!');
-// });
-
 const server = http.createServer(app).listen(5000, function (err) {
   if (err) {
     console.log(err);
